7f358083afdae747531b2ccf1d56a190986baf91
igoraresti <igoraresti@gmail.com>
2017-01-25 09:49:27 +0100

    Actualizado el modelo de datos

--- src/main/java/modelo/Contacto.java
+++ src/main/java/modelo/Contacto.java
@@ -1,13 +1,23 @@
 package modelo;
 
 public class Contacto {
-	
+
+	private int id;
 	private String nombre;
 	private String apellido;
-	
-	public Contacto(String nombre, String apellido){
+	private int telefono;
+	private char sexo;
+
+	public Contacto(String nombre, String apellido) {
 		this.apellido = apellido;
 		this.nombre = nombre;
+	}
+
+	public Contacto(String nombre, String apellido, int telefono, char sexo) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.sexo = sexo;
 	}
 
 	public String getNombre() {
@@ -25,7 +35,29 @@ public class Contacto {
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
 
 }
