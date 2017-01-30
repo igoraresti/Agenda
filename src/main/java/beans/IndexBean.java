8d2b4e900c8357ffae22bfd02942782042fa29ad
igoraresti <igoraresti@gmail.com>
2017-01-30 08:58:59 +0100

    cambios en local de index.html, indexbean y databaseSQL

--- src/main/java/beans/IndexBean.java
+++ src/main/java/beans/IndexBean.java
@@ -6,26 +6,46 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import database.Database;
 import database.DatabaseSQL;
 import modelo.Actor;
 
 @ManagedBean
-@ApplicationScoped
+@SessionScoped
 public class IndexBean implements Serializable {
 
 	private static final long serialVersionUID = -4901741514584007341L;
-	private List<Actor> actorList;
 	private DatabaseSQL database = new DatabaseSQL();
-
+	private String last_name_filter;
+	private List<Actor> lista_actores;
+	
 	@PostConstruct
-	public void init() {
-		actorList = database.getAllActors();
+	public void init(){
+		last_name_filter = "";
+		lista_actores = database.getAllActors(last_name_filter);
 	}
 
+/*
 	public List<Actor> getActorList() {
-		return actorList;
+		return database.getAllActors();
+	}*/
+	
+	public List<Actor> getActorList() {
+		lista_actores = database.getAllActors(last_name_filter);
+		System.out.println("El filtro es:" +last_name_filter);
+		return lista_actores;
 	}
+
+	public String getLast_name_filter() {
+		return last_name_filter;
+	}
+
+	public void setLast_name_filter(String last_name_filter) {
+		this.last_name_filter = last_name_filter;
+	}
+	
+	
 
 }
--- src/main/java/database/DatabaseSQL.java
+++ src/main/java/database/DatabaseSQL.java
@@ -9,63 +9,84 @@ import java.util.List;
 
 import modelo.Actor;
 
-public class DatabaseSQL implements DatabaseOperation{
+public class DatabaseSQL implements DatabaseOperation {
 
-private List<Actor> listaActores = new ArrayList<Actor>();
-private MySQLConexion msc;
-	
-	public DatabaseSQL(){
+	private List<Actor> listaActores = new ArrayList<Actor>();
+	private MySQLConexion msc;
+
+	public DatabaseSQL() {
 		msc = new MySQLConexion();
 	}
 
 	public void addActor(Actor contact) {
-		Connection con=msc.getConexion();
+		Connection con = msc.getConexion();
 		// create a Statement from the connection
 		Statement statement;
 		try {
 			statement = con.createStatement();
-			statement.executeUpdate("INSERT INTO Customers " + "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)");
+			statement.executeUpdate("INSERT INTO Customers "
+					+ "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		// insert the data
-		
-		
-		
+
 	}
 
-	public void deleteActor(int contactPosition){
-		
+	public void deleteActor(int contactPosition) {
+
 	}
 
 	public Actor getActor(int contactPosition) {
-	
-		 
-		 return null;
-		 
+
+		return null;
+
 	}
 
 	public List<Actor> getAllActors() {
-		
-		Connection con=msc.getConexion();
+
+		Connection con = msc.getConexion();
 		// create a Statement from the connection
-		
-		
-		 try {
-			Statement stmt= con.createStatement();
-			ResultSet rs= stmt.executeQuery("SELECT * FROM Actor");
-			
-			while (rs.next()){
-				Actor contact=new Actor(rs.getString("first_name"),rs.getString("last_name"));
+
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Actor");
+
+			while (rs.next()) {
+				Actor contact = new Actor(rs.getString("first_name"),
+						rs.getString("last_name"));
 				listaActores.add(contact);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 return listaActores;
-		
+		return listaActores;
+
+	}
+
+	public List<Actor> getAllActors(String last_name_filter) {
+
+		Connection con = msc.getConexion();
+		System.out.println("El filtro en database es: "+last_name_filter);
+		// create a Statement from the connection
+
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Actor where last_name like '"+last_name_filter+"%'");
+
+			while (rs.next()) {
+				Actor contact = new Actor(rs.getString("first_name"),
+						rs.getString("last_name"));
+				listaActores.add(contact);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listaActores;
+
 	}
 }
